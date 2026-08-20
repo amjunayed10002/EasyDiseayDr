8e08f17113c981d652b27ecbcbd9411c8d7e32ac
amjunayed10002 <amadnanmahmud10002@gmail.com>
2026-08-20 10:48:07 +0600

    Fix Vercel Express export interop

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,7 @@
-import app from "./server.cjs";
+import serverModule from "./server.cjs";
 import type { Request, Response } from "express";
+
+const app = typeof serverModule === "function" ? serverModule : serverModule.default;
 
 export default function handler(req: Request, res: Response) {
 	return app(req, res);
